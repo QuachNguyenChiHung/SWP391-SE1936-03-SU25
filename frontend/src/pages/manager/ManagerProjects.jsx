29ce185f7244c0d8dcc86c9354583e697b59e7bb
QuachNguyenChiHung <bacon3632@gmail.com>
2026-03-01 15:50:53 +0700

    fix: set default project type and improve error handling for project creation

--- frontend/src/pages/manager/ManagerProjects.jsx
+++ frontend/src/pages/manager/ManagerProjects.jsx
@@ -13,7 +13,7 @@ export const ManagerProjects = ({ user }) => {
     const [isCreateProjectModalOpen, setIsCreateProjectModalOpen] = useState(false);
     const [projectName, setProjectName] = useState('');
     const [projectDescription, setProjectDescription] = useState('');
-    const [projectType, setProjectType] = useState('');
+    const [projectType, setProjectType] = useState('Classification');
     const [projectDeadline, setProjectDeadline] = useState('');
     const [deadlineError, setDeadlineError] = useState('');
     const [projects, setProjects] = useState([]);
@@ -67,8 +67,11 @@ export const ManagerProjects = ({ user }) => {
             const p = await api.get(`/Projects/?pageNumber=${page}&pageSize=${pageLength}`);
             setProjects(p.data.data.items);
         } catch (error) {
-            alert("Failed to create project");
-            console.error("Error creating project:", error.response || error.message);
+            if (error.response) {
+                alert(error.response.data.errors || 'Failed to create project');
+            }
+
+            console.error(error.response.data.errors || error.message);
         }
 
         setProjectName('');
